91d22cbabfed675f4ac147545f2809e1c2d96f8a
Victor Burtsev <vb4oot@gmail.com>
2025-04-02 16:46:41 +0300

    logger is added to services.py

--- src/services.py
+++ src/services.py
@@ -2,10 +2,18 @@ import json
 import re
 import pandas as pd
 from pandas import DataFrame
-
+import logging
 
 pd.options.mode.copy_on_write = True
 
+# Создание объекта логера для записи событий
+logger = logging.getLogger("services")
+logger.setLevel(logging.DEBUG)
+# При запуске Pytest исправить путь к имени файла: "./logs/utils.log"
+file_handler = logging.FileHandler("./logs/services.log", "w", encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 def transfers_to_people(input_df: DataFrame) -> json:
     """Функция получает на вход DataFrame и возвращает JSON-ответ со всеми транзакциями,
@@ -16,6 +24,7 @@ def transfers_to_people(input_df: DataFrame) -> json:
     Сергей З.
     Артем П."""
 
+    logger.info("Начало функций transfers_to_people")
     # Фильтруем df по категории "переводы":
     transactions_df = input_df[input_df["Категория"] == "Переводы"]
     # Переводим отфильтрованный df в словарь:
@@ -30,7 +39,7 @@ def transfers_to_people(input_df: DataFrame) -> json:
             list_of_people.append(dict_)
     # переводим словарь в формат JSON:
     result_json = json.dumps(list_of_people, ensure_ascii=False, indent=4)
+    logger.info("Функция transfers_to_people завершена успешно")
     return result_json
 
 
-# print(transfers_to_people())
