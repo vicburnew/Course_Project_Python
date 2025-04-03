d599e0f994c08dc2142c2e6b16d676cbff9f8d80
Victor Burtsev <vb4oot@gmail.com>
2025-04-03 09:46:48 +0300

    tests are added for  spending_by_category in test_reports.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -249,3 +249,28 @@ def fixt_test_df_2():
             ],
         }
     )
+
+@pytest.fixture
+def result_spndg_by_cat_1():
+    return ('{\n'
+ '    "category": "переводы",\n'
+ '    "start_date": "2021-12-25 23:59:59",\n'
+ '    "end_date": "2021-09-26 23:59:59",\n'
+ '    "total_expenses": 6955.0\n'
+ '}')
+
+@pytest.fixture
+def result_spndg_by_cat_2():
+    return ('{\n'
+ '    "category": "переводы",\n'
+ '    "start_date": "2025-04-03 23:59:59",\n'
+ '    "end_date": "2025-01-03 23:59:59",\n'
+ '    "total_expenses": -0.0\n'
+ '}')
+
+
+@pytest.fixture
+def result_spndg_by_cat_3():
+    return
+
+
